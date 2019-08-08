5985a118352b718d4f5d9275359176467162774e
Edinson Requena <edinson17286@gmail.com>
2019-08-08 01:23:51 -0400

    first commit

---
+++ convertor.py
@@ -0,0 +1,21 @@
+import imageio
+import os
+
+video = os.path.abspath("diana.mp4")
+
+def gifMaker(inputPath, targetFormat):
+	outputPath = os.path.splitext(inputPath)[0] + targetFormat
+
+	print(f"converting {inputPath} \n to {outputPath}")
+
+	reader = imageio.get_reader(inputPath)
+	fps = reader.get_meta_data()["fps"]
+
+	writer = imageio.get_writer(outputPath, fps=fps)
+
+	for frames in reader:
+		writer.append_data(frames)
+		print(f"Frame {frames}")
+
+	print("Oh yeah!!!")
+	writer.close()
